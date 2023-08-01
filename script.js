7de94e55afb8b729807668ce2a7523c7ce83aa39
OliviaMkandawire <oliviamkandawire66@gmail.com>
2023-08-01 21:39:58 +0200

    quick update
    adding all local files

---
+++ script.js
@@ -0,0 +1,8 @@
+    function toggleNavigation() {
+        var navigation = document.getElementById('links');
+        if (navigation.style.display === 'block') {
+            navigation.style.display = 'none';
+        } else {
+            navigation.style.display = 'block';
+        }
+    }
